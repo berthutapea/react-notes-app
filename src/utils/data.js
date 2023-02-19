1f80ea598078f65efd4ed520b42e9ab4db1b5959
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-19 20:47:03 +0700

    data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,11 +1,11 @@
 const getInitialData = () => ([
-  // {
-  //   id: 1,
-  //   title: "Babel",
-  //   body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
-  //   createdAt: '2022-04-14T04:27:34.572Z',
-  //   archived: false,
-  // },
+  {
+    id: 1,
+    title: "Babel",
+    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
+    createdAt: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
   {
     id: 2,
     title: "Functional Component",
