f826bc14b272bb8b666938dec56386af5d1bb597
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-19 20:46:43 +0700

    data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,18 +1,18 @@
 const getInitialData = () => ([
-  {
-    id: 1,
-    title: "Babel",
-    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
-    createdAt: '2022-04-14T04:27:34.572Z',
-    archived: false,
-  },
   // {
-  //   id: 2,
-  //   title: "Functional Component",
-  //   body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
+  //   id: 1,
+  //   title: "Babel",
+  //   body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
   //   createdAt: '2022-04-14T04:27:34.572Z',
   //   archived: false,
   // },
+  {
+    id: 2,
+    title: "Functional Component",
+    body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
+    createdAt: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
   {
     id: 3,
     title: "Modularization",
