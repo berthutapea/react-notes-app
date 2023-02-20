b3fa3b594f14b618b2a836a190540db02efd514e
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-20 16:15:27 +0700

    Note Item Action

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -12,14 +12,14 @@ const NoteItem = ({ note, onDelete, onArchive }) => {
                 <p className="note-item__date">{showFormattedDate(note.createdAt)}</p>
                 <p className="note-item__body">{note.body}</p>
             </div>
-            <div className="note-item__action">
+            {/* <div className="note-item__action">
                 {
                     note.archived === false ?
                     <button className="note-item__archive-button" onClick={onArchiveClick}>Archive</button> :
                     <button className="note-item__archive-button" onClick={onArchiveClick}>Unarchive</button>
                 }
                 <button className="note-item__delete-button" onClick={onDeleteClick}>Delete</button>
-            </div>
+            </div> */}
         </div>
     )
 }
